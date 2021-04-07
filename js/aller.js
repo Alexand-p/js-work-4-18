1e86fa58eca2257d839cc9705acc01a244b40b5e
Alexand-p <3082303@gmail.com>
2021-04-07 21:42:10 +0300

    first commit

---
+++ js/aller.js
@@ -0,0 +1,52 @@
+// function testFactorial(a) {
+//     for(i = 7; i < 5040; i = i + 1 ){
+//       return i *= i
+     
+//     }
+//     console.log(i)
+// }
+
+
+
+// function testFactorial() {
+//   for (let i = 1 , b = 1 ; i < 5040; b = ++b) {
+//     i *=  b ;
+//     return i ;
+//   }
+  
+// }
+
+
+
+
+// function testFactorial1 (){
+//   let i = 1, b = 1;        5
+//   if (i < 5040) {
+//      b = ++b;
+//      i *= b
+//      return i ; 
+//   }
+// }
+
+
+// function testFactorial(a) {
+//   let x = 1 ;
+//   for (let i = 1 ; i < a ; i = ++i) {
+//     x = x*i;
+//   }
+//   return x;
+// }
+// var a = 5794675 ; 
+// var b = 181683 ; 
+// function testStr(a, b) {
+//   var v = a + b ; 
+//     return v
+//  }
+//  console.log(x.length); 
+
+
+var myString = new String("Hello world");
+console.log(myString.valueOf());
+
+
+
